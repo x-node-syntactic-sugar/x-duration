bf4d90c4cfab99243a1f6f662aa4c4f8c1b714ae
David Brown <david@devanco.com>
2023-04-07 14:19:36 +0200

    feat: change factory interface
    BREAKING CHANGE: `d(1).second` should be changed to `d(1, "second")`

--- src/__tests__/index.spec.ts
+++ src/__tests__/index.spec.ts
@@ -2,13 +2,12 @@ import d, { XDuration, XDurationFactory } from "../index";
 
 test("exports a default helper function", () => {
   expect(typeof d).toBe("function");
-  expect(d(1) instanceof XDurationFactory).toBe(true);
-  expect(d(1).second instanceof XDuration).toBe(true);
+  expect(d(1, "ms") instanceof XDuration).toBe(true);
 });
 
 test("exports XDurationFactory", () => {
   expect(typeof XDurationFactory).toBe("function");
-  expect(typeof new XDurationFactory(1)).toBe("object");
+  expect(typeof XDurationFactory(1, "ms")).toBe("object");
 });
 
 test("exports XDuration", () => {
@@ -20,14 +19,14 @@ describe("d.sum", () => {
   test("d.sum returns a duration summing the parts", () => {
     const duration = d.sum(
       // @ts-ignore
-      d(1).year +
-        d(1).month +
-        d(1).week +
-        d(1).day +
-        d(1).hour +
-        d(1).minute +
-        d(1).second +
-        d(1).millisecond
+      d(1, "year") +
+        d(1, "month") +
+        d(1, "week") +
+        d(1, "day") +
+        d(1, "hour") +
+        d(1, "minute") +
+        d(1, "second") +
+        d(1, "millisecond")
     );
     expect(duration.toObject()).toStrictEqual({
       years: 1,
@@ -44,7 +43,7 @@ describe("d.sum", () => {
   test("d.sum rescales result duration", () => {
     const duration = d.sum(
       // @ts-ignore
-      d(3).days + d(4).days
+      d(3, "days") + d(4, "days")
     );
     expect(duration.toObject()).toStrictEqual({ weeks: 1 });
   });
@@ -52,7 +51,7 @@ describe("d.sum", () => {
   test("d.sum handles subtraction", () => {
     const duration = d.sum(
       // @ts-ignore
-      d(1).week - d(3).day
+      d(1, "week") - d(3, "day")
     );
     expect(duration.toObject()).toStrictEqual({ days: 4 });
   });
--- src/__tests__/x-duration-factory.spec.ts
+++ src/__tests__/x-duration-factory.spec.ts
@@ -2,92 +2,92 @@ import { XDurationFactory } from "../x-duration-factory";
 
 describe("XDurationFactory", () => {
   test("XDurationFactory#millisecond returns a duration in milliseconds", () => {
-    const duration = new XDurationFactory(1).millisecond;
+    const duration = XDurationFactory(1, "millisecond");
     expect(duration.toObject()).toStrictEqual({ milliseconds: 1 });
   });
 
   test("XDurationFactory#milliseconds returns a duration in milliseconds", () => {
-    const duration = new XDurationFactory(2).milliseconds;
+    const duration = XDurationFactory(2, "milliseconds");
     expect(duration.toObject()).toStrictEqual({ milliseconds: 2 });
   });
 
   test("XDurationFactory#second returns a duration in seconds", () => {
-    const duration = new XDurationFactory(1).second;
+    const duration = XDurationFactory(1, "second");
     expect(duration.toObject()).toStrictEqual({ seconds: 1 });
   });
 
   test("XDurationFactory#seconds returns a duration in seconds", () => {
-    const duration = new XDurationFactory(2).seconds;
+    const duration = XDurationFactory(2, "seconds");
     expect(duration.toObject()).toStrictEqual({ seconds: 2 });
   });
 
   test("XDurationFactory#minute returns a duration in minutes", () => {
-    const duration = new XDurationFactory(1).minute;
+    const duration = XDurationFactory(1, "minute");
     expect(duration.toObject()).toStrictEqual({ minutes: 1 });
   });
 
   test("XDurationFactory#minutes returns a duration in minutes", () => {
-    const duration = new XDurationFactory(2).minutes;
+    const duration = XDurationFactory(2, "minutes");
     expect(duration.toObject()).toStrictEqual({ minutes: 2 });
   });
 
   test("XDurationFactory#hour returns a duration in hours", () => {
-    const duration = new XDurationFactory(1).hour;
+    const duration = XDurationFactory(1, "hour");
     expect(duration.toObject()).toStrictEqual({ hours: 1 });
   });
 
   test("XDurationFactory#hours returns a duration in hours", () => {
-    const duration = new XDurationFactory(2).hours;
+    const duration = XDurationFactory(2, "hours");
     expect(duration.toObject()).toStrictEqual({ hours: 2 });
   });
 
   test("XDurationFactory#day returns a duration in days", () => {
-    const duration = new XDurationFactory(1).day;
+    const duration = XDurationFactory(1, "day");
     expect(duration.toObject()).toStrictEqual({ days: 1 });
   });
 
   test("XDurationFactory#days returns a duration in days", () => {
-    const duration = new XDurationFactory(2).days;
+    const duration = XDurationFactory(2, "days");
     expect(duration.toObject()).toStrictEqual({ days: 2 });
   });
 
   test("XDurationFactory#week returns a duration in weeks", () => {
-    const duration = new XDurationFactory(1).week;
+    const duration = XDurationFactory(1, "week");
     expect(duration.toObject()).toStrictEqual({ weeks: 1 });
   });
 
   test("XDurationFactory#weeks returns a duration in weeks", () => {
-    const duration = new XDurationFactory(2).weeks;
+    const duration = XDurationFactory(2, "weeks");
     expect(duration.toObject()).toStrictEqual({ weeks: 2 });
   });
 
   test("XDurationFactory#month returns a duration in months", () => {
-    const duration = new XDurationFactory(1).month;
+    const duration = XDurationFactory(1, "month");
     expect(duration.toObject()).toStrictEqual({ months: 1 });
   });
 
   test("XDurationFactory#months returns a duration in months", () => {
-    const duration = new XDurationFactory(2).months;
+    const duration = XDurationFactory(2, "months");
     expect(duration.toObject()).toStrictEqual({ months: 2 });
   });
 
   test("XDurationFactory#quarter returns a duration in quarters", () => {
-    const duration = new XDurationFactory(1).quarter;
+    const duration = XDurationFactory(1, "quarter");
     expect(duration.toObject()).toStrictEqual({ quarters: 1 });
   });
 
   test("XDurationFactory#quarters returns a duration in quarters", () => {
-    const duration = new XDurationFactory(2).quarters;
+    const duration = XDurationFactory(2, "quarters");
     expect(duration.toObject()).toStrictEqual({ quarters: 2 });
   });
 
   test("XDurationFactory#year returns a duration in years", () => {
-    const duration = new XDurationFactory(1).year;
+    const duration = XDurationFactory(1, "year");
     expect(duration.toObject()).toStrictEqual({ years: 1 });
   });
 
   test("XDurationFactory#years returns a duration in years", () => {
-    const duration = new XDurationFactory(2).years;
+    const duration = XDurationFactory(2, "years");
     expect(duration.toObject()).toStrictEqual({ years: 2 });
   });
 });
